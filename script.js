0e26db8d5f5c4364e8b316ba9bc3be2e85a99317
Ahmad <Ahmadzuainat@gmail.com>
2024-09-14 19:44:40 +0300

    رسالة توضح التغييرات

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-}
-const toggleButton = document.getElementById('dark-mode-toggle');
-const htmlElement = document.documentElement;
+const burger = document.getElementById('burger');
+const dropdownMenu = document.getElementById('dropdown-menu');
 
-// تحقق إذا كان الوضع الداكن قد تم حفظه في التخزين المحلي
-if (localStorage.getItem('theme') === 'dark') {
-  htmlElement.classList.add('dark');
-}
-
-toggleButton.addEventListener('click', () => {
-  htmlElement.classList.toggle('dark');
-  
-  // حفظ التفضيل في التخزين المحلي
-  if (htmlElement.classList.contains('dark')) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
+burger.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('hidden');
 });
 
-
+document.addEventListener('click', (e) => {
+  if (!burger.contains(e.target) && !dropdownMenu.contains(e.target)) {
+    dropdownMenu.classList.add('hidden');
+  }
+});
